b744fd015e4f39820991f97a42615c2ca17cd6a0
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2025-06-01 14:27:47 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,45 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
+  features {
+    
+  }
+  # Para registrar client_id, client_secret e tenant_id, precisa entrar no entra id e registrar em app registrantion.
+  # Depois ao entrar no registro, entrar na opcao certificates & secrets para criar o client_secret
+  client_id = "9e56ddb8-e204-4cff-9c84-41ec1bb8f77a"
+  client_secret = "uYW8Q~VxRd_zZ.oXg-K0FOpY3r9Rd7cVdJABmbpv"
+  tenant_id = "fdc9d3b2-a5f3-44eb-9d7a-08c87344d26a"
+  subscription_id = "e0ff941f-00b9-4c7d-b5a2-947f45ff2ae0"
 }
 
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
   location = "East US"
+}
+
+
+resource "azurerm_storage_account" "appstore11984826150" {
+  name                     = "appstore11984826150"
+  resource_group_name      = azurerm_resource_group.appgrp.name
+  location                 = azurerm_resource_group.appgrp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "develop"
+  }
+}
+
+resource "azurerm_storage_container" "scripts" {
+  name                  = "scripts"
+  storage_account_id    = azurerm_storage_account.appstore11984826150.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "script01" {
+  name                   = "script01.sh"
+  storage_account_name   = azurerm_storage_account.appstore11984826150.name
+  storage_container_name = azurerm_storage_container.scripts.name
+  type                   = "Block"
+  source                 = "script01.sh"
 }
